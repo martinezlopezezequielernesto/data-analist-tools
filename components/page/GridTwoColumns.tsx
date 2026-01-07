045fa8ac14182ba302809ba095eb9d4b921b6536
martinezlopezezequielernesto <martinezlopezezequielernesto@gmail.com>
2026-01-06 23:26:39 -0300

    adaptando a dispositivos mas cortos

--- components/page/GridTwoColumns.tsx
+++ components/page/GridTwoColumns.tsx
@@ -8,7 +8,7 @@ type GridTwoColumnsProps = {
 
 export default function GridTwoColumns({ children }: GridTwoColumnsProps) {
   return (
-    <div className="w-full grid grid-cols-2 gap-8">
+    <div className="w-full grid grid-cols-1 grid-rows-2 lg:grid-cols-2 lg:grid-rows-1 gap-8">
       {children}
     </div>
   );
