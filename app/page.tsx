4dbb9ef58b3a259a676c3ccc9cb76ee316027eec
martinezlopezezequielernesto <martinezlopezezequielernesto@gmail.com>
2026-01-06 23:25:03 -0300

    texto base para pagina en construccion

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 
 export default function Page() {
   return (
-    <p className="">Home Page</p>
+    <p className="w-full h-full text-center flex justify-center items-center text-4xl font-bold">Home Page en construccion</p>
   );
 }
