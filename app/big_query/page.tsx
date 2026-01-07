aa1ad8c5e873ce27b4ba566f6083a37dc1f3bf5a
martinezlopezezequielernesto <martinezlopezezequielernesto@gmail.com>
2026-01-06 23:25:31 -0300

    modificacion del caracter final de bigquery

--- app/big_query/page.tsx
+++ app/big_query/page.tsx
@@ -23,7 +23,7 @@ export default function Page() {
     const lines = base.split("\n");
 
     // Agregar "/" al final de cada línea y unirlas con salto de línea
-    const formattedQuery = lines.map(line => line + " /").join("\n");
+    const formattedQuery = lines.map(line => line + " \\").join("\n");
 
     // Crear la variable query
     const query = formattedQuery;
@@ -37,13 +37,13 @@ export default function Page() {
 
   return (
     <PageContainer>
-      <Paragraph center text="Crea consultas de BigQuery rápidamente y optimiza tus tareas diarias de análisis de datos." />
+      {/*<Paragraph center text="Crea consultas de BigQuery rápidamente y optimiza tus tareas diarias de análisis de datos." />*/}
       <GridTwoColumns>
         <Stack>
           <div className="relative">
             <Textarea value={base} onChange={setBase} placeholder="Escribe aquí tu query..." rows={rows} />
             <button onClick={resetBase} className="absolute top-3 right-3 p-3 rounded-full bg-gray-300 hover:bg-green-600 transition-colors duration-300 cursor-pointer group">
-              <FiTrash2 size={24} className="text-gray-800 group-hover:text-white"/>
+              <FiTrash2 className="size-4 md:size-6 text-gray-800 group-hover:text-white"/>
             </button>
           </div>
           <Button title="Generar Variable" type="primary" onClick={handleClick} />
