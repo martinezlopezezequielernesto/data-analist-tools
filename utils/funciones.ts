882824b9a773a6e239aa05386000740f0ffe99d6
martinezlopezezequielernesto <martinezlopezezequielernesto@gmail.com>
2026-01-10 16:27:51 -0300

    funciones para capturar ciertos patrones.

--- utils/funciones.ts
+++ utils/funciones.ts
@@ -21,3 +21,54 @@ export function getHeaderRange(range: string): string {
 
   return `${startCol}${startRow}:${endCol}${startRow}`;
 }
+
+
+export function getFunctionName(code: string): string {
+  const match = code.match(/function\s+([a-zA-Z_$][\w$]*)/);
+  return match ? match[1].toString().trim() : "";
+}
+
+export function getIdSpreadsheetOrigen(text: string): string {
+  const regex = /idSpreadsheetOrigen\s*:\s*"([^"]*)"/;
+  const match = text.match(regex);
+  return match ? match[1] : "";
+}
+
+export function getNombreHojaOrigen(text: string): string {
+  const regex = /nombreHojaOrigen\s*:\s*"([^"]*)"/;
+  const match = text.match(regex);
+  return match ? match[1] : "";
+}
+
+export function getIdSpreadsheetDestino(text: string): string {
+  const regex = /idSpreadsheetDestino\s*:\s*"([^"]*)"/;
+  const match = text.match(regex);
+  return match ? match[1] : "";
+}
+
+export function getNombreHojaDestino(text: string): string {
+  const regex = /nombreHojaDestino\s*:\s*"([^"]*)"/;
+  const match = text.match(regex);
+  return match ? match[1] : "";
+}
+
+export function getNombresColumnasACopiarDeOrigen(text: string): string {
+  const regex = /nombresColumnasACopiarDeOrigen\s*:\s*(\[[^\]]*\])/;
+  const match = text.match(regex);
+  return match ? match[1] : "";
+}
+
+
+export function getRangoALimpiarEnDestino(text: string): string {
+  const regex = /rangoALimpiarEnDestino\s*:\s*"([^"]*)"/;
+  const match = text.match(regex);
+  return match ? match[1] : "";
+}
+/*
+  idSpreadsheetOrigen: string;
+  nombreHojaOrigen: string;
+  idSpreadsheetDestino: string;
+  nombreHojaDestino: string;
+  nombresColumnasACopiarDeOrigen: string[];
+  rangoALimpiarEnDestino: string;
+*/
